ac2e9d7ee307c671b0a9e6bbb11dd17f7ad2e4e0
Leo-Escobar5 <20000453@alumnos.utleon.edu.mx>
2024-08-26 18:32:41 -0600

    home agregado

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,11 +1,14 @@
 import { Component } from '@angular/core';
+import { MatIconModule } from '@angular/material/icon';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [],
+  imports: [
+    MatIconModule,
+  ],
   templateUrl: './home.component.html',
-  styleUrl: './home.component.css'
+  styleUrls: ['./home.component.css']
 })
 export class HomeComponent {
 
