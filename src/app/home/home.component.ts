72e902cd751a5b29b9c62bfb9f99b708cba6003c
Leo-Escobar5 <20000453@alumnos.utleon.edu.mx>
2024-08-27 23:03:23 -0600

    home mejorado

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -12,4 +12,11 @@ import { MatIconModule } from '@angular/material/icon';
 })
 export class HomeComponent {
 
+  scrollToSection(sectionId: string): void {
+    const element = document.getElementById(sectionId);
+    if (element) {
+      element.scrollIntoView({ behavior: 'smooth', block: 'start' });
+    }
+  }
+
 }
